bd7dc55817550c3b850751b249595146d589814a
chris <19cg0042@itsncg.edu.mx>
2022-03-23 16:59:31 -0700

    Actualizado en la pantalla

--- BACE/src/Classes/Buscar.java
+++ BACE/src/Classes/Buscar.java
@@ -1,4 +1,34 @@
 package Classes;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
 public class Buscar {
+
+    @FXML private Button buttonBuscar;
+
+    @FXML private TextField textFieldNombreId;
+
+    @FXML private VBox vBoxResultados;
+
+    @FXML void buscarAction(ActionEvent event) {
+
+    }
+
+    @FXML void buscarEntered(MouseEvent event) {
+
+    }
+
+    @FXML void buscarExited(MouseEvent event) {
+
+    }
+
+    @FXML void textFieldNombreIdAction(ActionEvent event) {
+
+    }
+
 }
--- BACE/src/Main.java
+++ BACE/src/Main.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -10,6 +11,7 @@ public class Main extends Application {
     @Override public void start(Stage primaryStage) throws Exception {
 
         primaryStage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Resources/menu.fxml")))));
+        primaryStage.getIcons().add(new Image("Images/logo.jpeg"));
         primaryStage.show();
 
     }//start
