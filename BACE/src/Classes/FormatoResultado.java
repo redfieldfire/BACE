c347a50e92b40df7e038595ad46394ec9a565fbf
chris <19cg0042@itsncg.edu.mx>
2022-03-23 09:28:21 -0700

    Actualizado en la pantalla

--- BACE/src/Classes/FormatoResultado.java
+++ BACE/src/Classes/FormatoResultado.java
@@ -1,4 +1,14 @@
 package Classes;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class FormatoResultado {
-}
+
+    @FXML private Text apellidos;
+
+    @FXML private Text id;
+
+    @FXML private Text nombre;
+
+}//FormatoResultado
